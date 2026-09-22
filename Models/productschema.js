4a3f48bdf02c5a0f3c2c0af9cfac1d195ec56717
codervinitjangir <vinitjangirr@gmail.com>
2026-09-22 11:14:52 +0530

    Update productSchema with brand, stock, and rating fields

--- Models/productschema.js
+++ Models/productschema.js
@@ -3,7 +3,11 @@ const mongoose = require("mongoose");
 const productSchema = new mongoose.Schema({
     name: { type: String, required: true },
     price: { type: Number, required: true },
-    category: { type: String, required: true }
+    category: { type: String, required: true },
+    brand: { type: String, required: true },
+    stock: { type: Number, required: true },
+    rating: { type: Number, required: true }
+    
 });
 
 module.exports = mongoose.model("Product", productSchema);
