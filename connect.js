5cac64ce0b5be3879ffc9420ddba4c53c0d2fc87
codervinitjangir <vinitjangirr@gmail.com>
2026-09-22 12:00:12 +0530

    Use .env for MongoDB Atlas connection (dotenv setup)

--- connect.js
+++ connect.js
@@ -1,7 +1,11 @@
 const mongoose = require("mongoose");
+const dns = require("dns");
+require("dotenv").config();
+
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 function connect(){
-    mongoose.connect("mongodb://127.0.0.1:27017/test")
+    mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log("Database connected"))
     .catch((err) => console.log(err));
 }
